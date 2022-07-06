dd8a7d4eb87daf5ae717848e5b3cfe56910664e2
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-07-06 21:21:22 +0200

    Finished writing the help command

--- ProtocollTool/ProtocollTool.cpp
+++ ProtocollTool/ProtocollTool.cpp
@@ -316,46 +316,106 @@ int main()
         }
         else if (command == "h" || command == "-h" || command == "help")
         {
-        logger << "This programm can be used by typing one of the following commands, where (...) indicates that the expression ";
-        logger << "inside the parenthesis can be used as an abbreviation of the command or parameter.\n";
-        logger << "(n)ew, (f)ind, (f)ilter, (s)how, (a)dd_data, (d)etails, (t)ags, (q)uit, (c)reate_mode, (del)ete_mode, (act)ivate_mode, (deac)tivate_mode, (modes), (u)pdate and (o)pen.\n\n";
-        string argument;
-        if (iss) 
-        {
-            iss >> argument;
-            if (argument == "n" || argument == "new")
+            logger << "This programm can be used by typing one of the following commands, where (...) indicates that the expression ";
+            logger << "inside the parenthesis can be used as an abbreviation of the command or parameter.\n";
+            logger << "(n)ew, (f)ind, (f)ilter, (s)how, (a)dd_data, (d)etails, (t)ags, (q)uit, (c)reate_mode, (del)ete_mode, (act)ivate_mode, (deac)tivate_mode, (modes), (edit)_mode, (u)pdate and (o)pen.\n";
+            logger << "Detailed information about a command can be found by running: help command.\n\n";
+            string argument;
+            if (iss) 
             {
-                logger << "(n)ew: date tag1 tag2 ... [-d]\nCreates a new note file and the optional parameter -d indicates that a data folder will be created. The given date can be in ";
-                logger << "the format of dd or dd.mm or dd.mm.yyyy (where one digit numebers can be specified as one digit or two digits with leading zero) or the special format t for today and ";
-                logger << "y for yesterday.\n\n";
+                iss >> argument;
+                if (argument == "n" || argument == "new")
+                {
+                    logger << "(n)ew: date tag1 tag2 ... [-d]\nCreates a new note file and the optional parameter -d indicates that a data folder will be created. The given date can be in ";
+                    logger << "the format of dd or dd.mm or dd.mm.yyyy (where one digit numebers can be specified as one digit or two digits with leading zero) or the special format t for today and ";
+                    logger << "y for yesterday.\n\n";
+                }
+                else if (argument == "f") {
+                    logger << "The abbreviation f will be interpreted as find if no note is currently selected and as filter, if multiple (or one) note(s) are selected.\n\n";
+                } 
+                else if (argument == "find") {
+                    logger << "(f)ind: [-(d)ate start_date-end_date] [-(c)ontains_(t)ags tag1 ...] [-(c)ontains_(a)ll_(t)ags tag1 ...] [-(n)o_(t)ags tag1 ...] [-(r)eg_(t)ext text] [-(v)ersions start_version-end_version] [-(d)ata_(o)nly]\n";
+                    logger << "Looks for notes that fall within the specified ranges (start and end dates are included in the interval). -ct describes a list of tags where one of them ";
+                    logger << "needs to be found in the note, while -cat describes a list of tags which all need to be find in the note. -nt describes a list of tags that are not allowed ";
+                    logger << "to be in matching notes and -rt stands for regular expression text, which will be used to search the content of the note. -v gives an interval of versions (a-...) ";
+                    logger << "that are considered and -do stands for data_only, meaning only notes with a data folder will be selected.\n\n";
+                }
+                else if (argument == "filter") {
+                    logger << "(f)ilter: [-(d)ate start_date-end_date] [-(c)ontains_(t)ags tag1 ...] [-(c)ontains_(a)ll_(t)ags tag1 ...] [-(n)o_(t)ags tag1 ...] [-(r)eg_(t)ext text] [-(v)ersions start_version-end_version] [-(d)ata_(o)nly]\n";
+                    logger << "Reduces the current selection of notes with parameter similar to find. Also, intevals contain the start and end values. -ct describes a list of tags where one of them ";
+                    logger << "needs to be found in the note, while -cat describes a list of tags which all need to be find in the note. -nt describes a list of tags that are not allowed ";
+                    logger << "to be in matching notes and -rt stands for regular expression text, which will be used to search the content of the note. -v gives an interval of versions (a-...) ";
+                    logger << "that are considered and -do stands for data_only, meaning only notes with a data folder will be selected.\n\n";
+                }
+                else if (argument == "s" || argument == "show") {
+                    logger << "(s)how: [(t)ags] [(m)etadata] [table_of_(c)ontent] [(d)ata] [(h)ide_(d)ate] [open_(i)mages] [open_as_(html)] [open_as_(tex)] [open_as_(pdf)] [open_as_(docx)] [open_as_(m)ark(d)own]\n";
+                    logger << "Shows the current note selection (created by find and filter) in various formats (specified by the open_as_... parameter), where by default only the date and content of the note are shown.";
+                    logger << "The parameters (t)ags, (m)etadata and table_of_(c)ontent add additional information to the output and (d)ata opens the data folders in the explorer. ";
+                    logger << "The command open_(i)mages additionally opens .jpg, .jpeg and .png files inside the data folders directly.\n\n";
+                }
+                else if (argument == "a" || argument == "add_data") {
+                    logger << "(a)dd_data:\nAdds a data folder for the notes that are currently in the selection. The selection is created by the find command and it can be refined by the filter command.\n\n";
+                }
+                else if (argument == "d" || argument == "details" ||argument == "show_details") {
+                    logger << "(d)etails: [-(t)ags] [-(p)ath] [-(l)ong_(p)ath] [-last_(m)odified] [-(c)ontent]\n";
+                    logger << "Shows details about the current selection of notes. The parameters controll what is shown.\n\n";
+                }
+                else if (argument == "t" || argument == "tags") {
+                    logger << "(t)ags:\nShows the statictics of tags of the current selection. If no selection was made, it shows the statistic of all tags of all notes.\n\n";
+                }
+                else if (argument == "q" || argument == "quit") {
+                    logger << "(q)uit:\nEnds the programm.";
+                }
+                else if (argument == "c" || argument == "create_mode") {
+                    logger << "(c)reate_mode: mode_name tag1 tag2 ... [-(o)pen_as format] [-(s)how_(t)ags] [-(s)how_(m)etadata] [-(s)how_table_of_(c)ontent] [-(s)how_(d)ata] ";
+                    logger << "[-(s)how_(h)ide_(d)ate] [-(s)how_open_(i)mages] [-(s)how_open_as_(html)] [-(s)how_open_as_(m)ark(d)own] [-(s)how_open_as_(docx)] ";
+                    logger << "[-(s)how_open_as_(pdf)] [-(s)how_open_as_la(tex)] [-(d)etail_(t)ags] [-(d)etail_(p)ath] [-(d)etail_(l)ong_(p)ath] ";
+                    logger << "[-(d)etail_(l)ast_(m)odified] [-(d)etail_(c)ontent]\n\n";
+                    logger << "A mode allows you to set the standard behavior of the (s)how and (d)etail command, as well as influence the (n)ew, (f)ind and (f)ilter commands. ";
+                    logger << "Options starting with show in the name control the (s)how command and options starting with detail control the (d)etails command. All set ";
+                    logger << "options will always be added to the commands when the mode is active. The mode tags will be added to the tags that are manually specified in the (n)ew command ";
+                    logger << "and are also added to the (f)ind and (f)ilter command in the -(c)ontails_(a)ll_(t)ags parameter, meaning only notes contraining all mode tags are selected.\n";
+                    logger << "Modes can be activated by the (act)ivate commandand and deactivated by the ";
+                    logger << "(deac)tivate command. Additionally, an existing mode can be edited by the (edit)_mode command and deleted by the (delete)_mode command. ";
+                    logger << "An overview over all modes is given by the (modes) command, which lists the name, open format, tags and all options of all modes.\n\n";
+                }
+                else if (argument == "del" || argument == "delete_mode") {
+                    logger << "(del)ete_mode: mode_name\n";
+                    logger << "Deletes the specified mode. More about modes can be found by running help create_mode.\n\n";
+                }
+                else if (argument == "act" || argument == "activate" || argument == "activate_mode") {
+                    logger << "(act)ivate_mode: mode_name\n";
+                    logger << "Activates the specified mode, such that all set options are now in effect. More about modes can be found by running help create_mode\n\n";
+                }
+                else if (argument == "deac" || argument == "deactivate" || argument == "deactivate_mode") {
+                    logger << "(deac)tivate_mode:\n";
+                    logger << "Deactivate the current mode and uses standard options again. More about modes can be found by running help create_mode\n\n";
+                }
+                else if (argument == "modes") {
+                    logger << "(modes):\n";
+                    logger << "Gives an overview over all existing modes and shows mode name, mode tags, default open format, mode options.\n\n";
+                }
+                else if (argument == "edit" || argument == "edit_mode") {
+                    logger << "(edit)_mode: mode_name [-(add_opt)ion opt1 opt2 ...] [-(remove_opt)ion opt1 opt2 ...] [-change_format open_as_format] [-(add_t)ags tag1 tag2 ...] [-(remove_t)ags tag1 tag2] [-change_name new_mode_name]\n";
+                    logger << "Where opt are the options from the (c)reate_mode command. More about modes can be found by running help create_mode\n\n";
+                }
+                else if (argument == "u" || argument == "update") {
+                    logger << "(u)pdate:\n";
+                    logger << "Updates the tag list, tag statistics and finds externally added files. Restarting the programm does the same trick.\n\n";
+                }
+                else if (argument == "o" || argument == "open") {
+                    logger << "(o)pen:\n";
+                    logger << "Opens the current selection of notes as markdown files, which can be edited by the editor of choice.\n\n";
+                }
+                else {
+                    logger << "command " << argument << " could not be found. It should be one of the following:\n";
+                    logger << "(n)ew, (f)ind, (f)ilter, (s)how, (a)dd_data, (d)etails, (t)ags, (q)uit, (c)reate_mode, (del)ete_mode, (act)ivate_mode, (deac)tivate_mode, (modes), (edit)_mode, (u)pdate and (o)pen.\n";
+                    logger << "Detailed information about a command can be found by running: help command.\n\n";
+                }
             }
-            else if (argument == "f") {
-                logger << "The abbreviation f will be interpreted as find if no note is currently selected and as filter, if multiple (or one) note(s) are selected.\n\n";
-            } 
-            else if (argument == "find") {
-                logger << "(f)ind: [-(d)ate start_date-end_date] [-(c)ontains_(t)ags tag1 ...] [-(c)ontains_(a)ll_(t)ags tag1 ...] [-(n)o_(t)ags tag1 ...] [-(r)eg_(t)ext text] [-(v)ersions start_version-end_version] [-(d)ata_(o)nly]\n";
-                logger << "Looks for notes that fall within the specified ranges (start and end dates are included in the interval). -ct describes a list of tags where one of them ";
-                logger << "needs to be found in the note, while -cat describes a list of tags which all need to be find in the note. -nt describes a list of tags that are not allowed ";
-                logger << "to be in matching notes and -rt stands for regular expression text, which will be used to search the content of the note. -v gives an interval of versions (a-...) ";
-                logger << "that are considered and -do stands for data_only, meaning only notes with a data folder will be selected.\n\n";
-            }
-            else if (argument == "filter") {
-                logger << "(f)ilter: [-(d)ate start_date-end_date] [-(c)ontains_(t)ags tag1 ...] [-(c)ontains_(a)ll_(t)ags tag1 ...] [-(n)o_(t)ags tag1 ...] [-(r)eg_(t)ext text] [-(v)ersions start_version-end_version] [-(d)ata_(o)nly]\n";
-                logger << "Reduces the current selection of notes with parameter similar to find. Also, intevals contain the start and end values. -ct describes a list of tags where one of them ";
-                logger << "needs to be found in the note, while -cat describes a list of tags which all need to be find in the note. -nt describes a list of tags that are not allowed ";
-                logger << "to be in matching notes and -rt stands for regular expression text, which will be used to search the content of the note. -v gives an interval of versions (a-...) ";
-                logger << "that are considered and -do stands for data_only, meaning only notes with a data folder will be selected.\n\n";
-            }
-            else if (argument == "s" || argument == "show") {
-                logger << "(s)how: [(t)ags] [(m)etadata] [table_of_(c)ontent] [(d)ata] [(h)ide_(d)ate] [open_(i)mages] [open_as_(html)] [open_as_(tex)] [open_as_(pdf)] [open_as_(docx)] [open_as_(m)ark(d)own]\n";
-                logger << "Shows the current note selection (created by find and filter) in various formats (specified by the open_as_... parameter), where by default only the date and content of the note are shown.";
-                logger << "The parameters (t)ags, (m)etadata and table_of_(c)ontent add additional information to the output and (d)ata opens the data folders in the explorer. ";
-                logger << "The command open_(i)mages additionally opens .jpg, .jpeg and .png files inside the data folders directly.\n\n";
-            }
-        }
         
         
-        // TODO: Finish help, add help to individual commands
+        
         }
         else {
             logger << "Command " << command << " could not be parsed. Available options are: (n)ew, (f)ind, (f)ilter, (s)how, (a)dd_data, (d)etails, (t)ags, (u)pdate, (e)xport and (o)pen." << 'n';
